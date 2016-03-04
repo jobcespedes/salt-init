c3e56642c3986141e84dcc2f8cea6c16e0334f45
Chris Geroux <chris.geroux@ace-net.ca>
2016-03-04 12:30:29 -0400

    Added minion ip arguments

--- salt-init.py
+++ salt-init.py
@@ -3,26 +3,45 @@ from __future__ import print_function
 from subprocess import Popen, PIPE
 import time
 import optparse as op
+
 def parseOptions():
   """Parses command line options
   
   """
   
-  parser=op.OptionParser(usage="Usage: %prog [options] EXPECTEDMINIONS"
+  parser=op.OptionParser(usage="Usage: %prog [options] EXPECTEDMINIONS EXPECTEDMINIONSIPS"
     ,version="%prog 1.0",description="waits for all expected minions to connect"
     +" then then provisions them with salt")
   
   #parse command line options
   return parser.parse_args()
+def isStrAnIp(string):
+  pattern=re.compile("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+  return pattern.match(string)
 def main():
   
   #parse command line options
   (options,args)=parseOptions()
-
-  expectedMinions=args
+  
+  minionIps=[]
+  minionHostNames=[]
+  for arg in args:
+    if isStrAnIP(arg):
+      minionIps.append(arg)
+    else: 
+      minionHostNames.append(arg)
+      
+  #check that minion host names and IPs are the same length
+  if len(minionIPs)!=len(minionHostNames):
+    raise Exception("Do not have same number of minion IPs "+str(minionIPS)
+      +" as minion host names "+str(minionHostNames))
+  
+  expectedMinions=minionHostNames
+  
+  #TODO: try pining each minion
   
   maxAttempts=100
-  timeToWaitBetweenAttempts=2
+  timeToWaitBetweenAttempts=10
   count=0
   allMinionsPresent=False
   cmd=["sudo","salt-run","manage.up"]
@@ -58,13 +77,18 @@ def main():
     minonsString+=expectedMinions[len(expectedMinions)-1]+"\'"
     
     print("All minions present, sending command to go to highstate")
-  
+    
+    #run high state command
     cmd=["sudo","salt","-L",minonsString,"state.highstate"]
     process=Popen(cmd,stdout=PIPE,stderr=PIPE)
     stdout,stderr=process.communicate()
     print(stdout)
     #print(stderr)
+    
+    #TODO: should also now disable accept all keys as we have all that we want
   else:
+    #TODO: seems even failed ssh's into missing minions allows them to suddenly 
+    #be detected, maybe I should try pining them
     print("Not all expected minions present after "+str(maxAttempts)
       +" checks every "+str(timeToWaitBetweenAttempts)+" seconds. Not "
       +"executing \"salt '*' state.highstate\". Check that all expected "
